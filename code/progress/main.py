b7398e4a361352b7e63d17f55c320e4dfa11e358
Frans de Boer <frans-de-boer@hotmail.com>
2022-12-03 13:44:53 +0100

    printing experiment information at start

--- code/progress/main.py
+++ code/progress/main.py
@@ -39,6 +39,9 @@ def main():
     args = parse_arguments()
     num_frames = args.num_segments * args.frames_per_segment
 
+    print(f'[Running on {device}]')
+    print(f'[{num_frames} frames per sample]')
+
     trainset = ProgressDataset(
         f'./data/{args.dataset}',
         num_segments=args.num_segments,
