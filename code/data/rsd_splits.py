80ca0853ce788fc70abc179772ce64a559f4eae0
Frans de Boer <frans-de-booer@hotmail.com>
2023-03-06 14:43:18 +0000

    method to create rsd t1/t2/v/e split files

---
+++ code/data/rsd_splits.py
@@ -0,0 +1,42 @@
+import os
+import argparse
+import random
+
+def save(target: str, name: str, index: int, items: list[int]):
+    with open(os.path.join(target, f'{name}_{index}.txt'), 'w+') as f:
+        f.writelines('\n'.join([f'video{item:02d}' for item in items]))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--t1', type=int, default=27)
+    parser.add_argument('--t2', type=int, default=27)
+    parser.add_argument('--v', type=int, default=7)
+    parser.add_argument('--e', type=int, default=19)
+    parser.add_argument('--num_splits', type=int, default=4)
+    parser.add_argument('--dataset', type=str)
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+
+    target = os.path.join(args.dataset, 'splitfiles')
+    total = args.t1 + args.t2 + args.v + args.e
+
+    for split_index in range(args.num_splits):
+        indices = list(range(total))
+        random.shuffle(indices)
+
+        t1 = indices[:args.t1]
+        t2 = indices[args.t1:args.t1+args.t2]
+        v = indices[args.t1 + args.t2 : args.t1 + args.t2 + args.v]
+        e = indices[args.t1 + args.t2 + args.v : args.t1 + args.t2 + args.v + args.e]
+
+        save(target, 't1', split_index, t1)
+        save(target, 't2', split_index, t2)
+        save(target, 't1_t2', split_index, t1+t2)
+        save(target, 'v', split_index, v)
+        save(target, 'e', split_index, e)
+
+
+if __name__ == '__main__':
+    main()
