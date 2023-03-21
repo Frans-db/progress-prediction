dda40c1532f04617bda24d8c73cbfad61b5c76f7
Frans de Boer <frans-de-booer@hotmail.com>
2023-03-21 10:15:32 +0000

    data vis

--- code/test.py
+++ code/test.py
@@ -1,8 +1,28 @@
-import torch
-import torch.nn as nn
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = '/home/frans/Datasets/cholec80/rgb-images'
+save_path = '/home/frans/Downloads/figure.png'
+video_names = sorted(os.listdir(path))
+lengths = []
+bars = {}
+for i in range(12):
+    bars[i*10] = 0
+
+for video_name in video_names:
+    video_path = os.path.join(path, video_name)
+    frames = sorted(os.listdir(video_path))
+    length = len(frames) / 60
+    lengths.append(length)
+    rounded_length = round(length / 10) * 10
+
+    bars[rounded_length] += 1
+
+print(np.quantile(lengths, 0.25), np.quantile(lengths, 0.5), np.quantile(lengths, 0.75))
+plt.bar([key for key in bars], [bars[key] for key in bars])
+plt.savefig(save_path)
 
 
-loss = nn.L1Loss()
-inputs = torch.FloatTensor([1, 1, 1])
-outputs = torch.FloatTensor([2, 2, 2])
-print(loss(inputs, outputs))
+
+
