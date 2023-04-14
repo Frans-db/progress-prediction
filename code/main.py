3bb41535148f5df1609c9a97875549c5df8fcda2
Frans de Boer <frans-de-booer@hotmail.com>
2023-04-14 14:57:20 +0000

    added arguments & experiment initialization

--- code/main.py
+++ code/main.py
@@ -1,11 +1,59 @@
-def main():
-    print(':)')
+import random
+import torch
+import numpy as np
+import argparse
+import wandb
+import json
+import os
 
-    # get arguments
-    # get device
-    # initialize
-        # wandb
-        # config json
+from utils import parse_args, get_device
+
+def init(args: argparse.Namespace) -> None:
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    config = {
+        # experiment
+        'seed': args.seed,
+        'experiment_name': args.experiment_name,
+        # network
+        'network': args.network,
+        # dataset
+        'train_set': args.train_set,
+        'test_set': args.test_set,
+        'train_split': args.train_split,
+        'test_split': args.test_split,
+        'data_type': args.data_type,
+        # training
+        'iterations': args.iterations,
+        'lr': args.lr,
+        'lr_decay_every': args.lr_decay_every,
+        'lr_decay': args.lr_decay,
+        'augmentations': args.augmentations,
+        'data_modifier': args.data_modifier,
+        # testing
+        'test_every': args.test_every,
+    }
+
+    if not args.wandb_disable:
+        wandb.init(
+            project=args.wandb_project,
+            group=args.wandb_group,
+            tags=args.wandb_tags,
+            config=config
+        )
+    if args.experiment_name:
+        experiment_root = os.path.join(args.data_root, 'experiments', args.experiment_name)
+        os.mkdir(experiment_root)
+        os.mkdir(os.path.join(experiment_root, 'results'))
+        with open(os.path.join(experiment_root, 'config.json'), 'w+') as f:
+            json.dump(config, f)
+
+def main() -> None:
+    args = parse_args()
+    device = get_device(args.device)
+    init(args)
 
     # get train & test set (function)
         # get (data) augmentations
---
+++ code/utils.py
@@ -0,0 +1,42 @@
+import argparse
+import torch
+
+def parse_args() -> argparse.Namespace:
+    networks = ['progressnet']
+
+    parser = argparse.ArgumentParser()
+    # experiment
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--experiment_name', type=str, default=None)
+    parser.add_argument('--data_root', type=str, default='/home/frans/Datasets/')
+    # wandb
+    parser.add_argument('--wandb_disable', action='store_true')
+    parser.add_argument('--wandb_project', type=str, default='mscfransdeboer')
+    parser.add_argument('--wandb_group', type=str, default=None)
+    parser.add_argument('--wandb_tags', nargs='+', default=None)
+    # network
+    parser.add_argument('--network', type=str, choices=networks)
+    # dataset
+    parser.add_argument('--train_set', type=str)
+    parser.add_argument('--test_set', type=str)
+    parser.add_argument('--train_split', type=str)
+    parser.add_argument('--test_split', type=str)
+    parser.add_argument('--data_type', type=str)
+    parser.add_argument('--data_mode', type=str, default='sequential', choices=['sequential', 'individual'])
+    parser.add_argument('--data_modifier', type=str, default=None, choices=['random', 'ones', 'indices'])
+    # training
+    parser.add_argument('--iterations', type=int, default=1500)
+    parser.add_argument('--lr', type=float, default=3e-3)
+    parser.add_argument('--lr_decay_every', type=int, default=500)
+    parser.add_argument('--lr_decay', type=float, default=1/2)
+    parser.add_argument('--augmentations', nargs='+', default='', choices=['subsection', 'subsample'])
+    # testing
+    parser.add_argument('--test_every', type=int, default=250)
+
+    return parser.parse_args()
+
+def get_device(device: str) -> torch.device:
+    if torch.cuda.is_available() and device == 'cuda':
+        return torch.device('cuda')
+    return torch.device('cpu')
