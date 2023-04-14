c507f350ed9ba4683cc2590cb562100b8f051803
Frans de Boer <frans-de-booer@hotmail.com>
2023-04-14 13:35:08 +0000

    basic code layout

---
+++ code/main.py
@@ -0,0 +1,22 @@
+def main():
+    print(':)')
+
+    # get arguments
+    # get device
+    # initialize
+        # wandb
+        # config json
+
+    # get train & test set (function)
+        # get (data) augmentations
+        # get sample augmentations
+    # get network (function)
+        # init network
+            # for parameters: xavier / random
+            # for static: depending on train set 
+    # get optimizer & scheduler
+
+    # training
+
+if __name__ == '__main__':
+    main()
