a9ca258721b521493d5f1d7b0ce125dacbe90e78
Frans de Boer <frans-de-booer@hotmail.com>
2023-04-14 14:57:42 +0000

    started dataset functionality

---
+++ code/datasets/__init__.py
@@ -0,0 +1,14 @@
+# datasets
+# ucf24 (action tubes from a pkl database)
+    # sequences
+    # 2d only
+# progress images
+    # sequences
+    # 2d only
+# progress text
+    # sequences
+    # 2d only
+
+# can a wrapper be made to turn sequences into 2d? 
+    # probably not because sequences would load all images when we only need 1
+# can progress images and text be combined?
