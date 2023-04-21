ceb962bf92c860f6c75dfd9362e97375b47d3602
Frans de Boer <frans-de-booer@hotmail.com>
2023-04-21 21:30:10 +0000

    removed antialias keyword

--- code/datasets/get_datasets.py
+++ code/datasets/get_datasets.py
@@ -51,7 +51,7 @@ def get_transform(args):
     transform = []
     if args.data_type == 'rgb-images':
         transform.append(transforms.ToTensor())
-        transform.append(transforms.Resize((300, 300), antialias=True))
+        transform.append(transforms.Resize((300, 300)))
     return transforms.Compose(transform)
 
 
