af8ed2428815d074992c9d40db8d4d1f4fea9998
Frans de Boer <frans-de-booer@hotmail.com>
2023-04-24 13:24:45 +0000

    removed scripts

--- code/scripts/resnet.sh
+++
@@ -1,5 +0,0 @@
-for seed in "$@"
-do
-python main.py --seed $seed --network progressnet_resnet --train_set ucf24 --test_set ucf24 --data_type rgb-images --iterations 10000 --loss l1 --bo --lr 1e-4 --lr_decay_every 100000 --lr_decay 1 --subsection_chance 0.0 --subsample_chance 0.0 --test_every 1000 --wandb_group "progressnet resnet"
-python main.py --seed $seed --data_type rgb-images --network progressnet_resnet --wandb_group "progressnet resnet
-done
