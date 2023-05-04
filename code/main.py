99fe4eb00bd8b67e25c0fdc6d8000fb33f209429
Frans de Boer <frans-de-booer@hotmail.com>
2023-05-04 17:33:43 +0000

    config log, rsdnet sbatch

--- code/main.py
+++ code/main.py
@@ -29,21 +29,42 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--data_dir", type=str, default="features/i3d_embeddings")
     parser.add_argument("--flat", action="store_true")
     parser.add_argument("--bboxes", action="store_true")
+    parser.add_argument("--subsample", action="store_true")
     parser.add_argument(
         "--rsd_type", type=str, default="none", choices=["none", "minutes", "seconds"]
     )
+    parser.add_argument('--fps', type=int, default=None)
     parser.add_argument("--train_split", type=str, default="t12_0.txt")
     parser.add_argument("--test_split", type=str, default="e_0.txt")
-    parser.add_argument("--subsample", action="store_true")
     # training
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--iterations", type=int, default=503 * 60)
+    # network
     parser.add_argument(
-        "--loss", type=str, default="l2", choices=["l2", "l1", "smooth_l1"]
+        "--network", type=str, default="progressnet", choices=["ute", "progressnet", "rsdnet"]
     )
+    parser.add_argument(
+        "--backbone",
+        type=str,
+        default=None,
+        choices=["vgg16", "resnet18", "resnet152"],
+    )
+    parser.add_argument("--load_backbone", type=str, default=None)
+    # network parameters
+    parser.add_argument("--feature_dim", type=int, default=64)
+    parser.add_argument("--embed_dim", type=int, default=1024)
+    parser.add_argument("--dropout_chance", type=str, default=0.5)
+    parser.add_argument("--pooling_layers", nargs="+", type=int, default=[1, 2, 3])
+    parser.add_argument("--roi_size", type=int, default=4)
+    # network loading
+    parser.add_argument("--load_experiment", type=str, default=None)
+    parser.add_argument("--load_iteration", type=int, default=None)
     # optimizer
     parser.add_argument(
         "--optimizer", type=str, default="adam", choices=["adam", "sgd"]
+    )
+    parser.add_argument(
+        "--loss", type=str, default="l2", choices=["l2", "l1", "smooth_l1"]
     )
     parser.add_argument("--momentum", type=float, default=0.9)
     parser.add_argument("--beta1", type=float, default=0.9)
@@ -53,31 +74,12 @@ def parse_args() -> argparse.Namespace:
     # scheduler
     parser.add_argument("--lr_decay", type=float, default=0.1)
     parser.add_argument("--lr_decay_every", type=int, default=503 * 30)
-    # network
-    parser.add_argument(
-        "--network", type=str, default="ute", choices=["ute", "progressnet", "rsdnet"]
-    )
-    parser.add_argument(
-        "--backbone",
-        type=str,
-        default="vgg16",
-        choices=["vgg16", "resnet18", "resnet152"],
-    )
-    parser.add_argument("--load_backbone", type=str, default=None)
-    # network loading
-    parser.add_argument("--load_experiment", type=str, default=None)
-    parser.add_argument("--load_iteration", type=int, default=None)
-    # network parameters
-    parser.add_argument("--feature_dim", type=int, default=64)
-    parser.add_argument("--embed_dim", type=int, default=20)
-    parser.add_argument("--dropout_chance", type=str, default=0.5)
-    parser.add_argument("--pooling_layers", nargs="+", type=int, default=[1, 2, 3])
-    parser.add_argument("--roi_size", type=str, default=4)
     # logging
     parser.add_argument("--log_every", type=int, default=250)
     parser.add_argument("--test_every", type=int, default=1000)
 
     parser.add_argument("--print_only", action="store_true")
+
     return parser.parse_args()
 
 
@@ -134,23 +136,41 @@ def main():
             name=args.wandb_name,
             tags=args.wandb_tags,
             config={
-                "dataset": args.dataset,
-                "train_split": args.train_split,
-                "test_split": args.test_split,
-                "batch_size": args.batch_size,
-                "lr": args.lr,
-                "weight_decay": args.weight_decay,
-                "lr_decay": args.lr_decay,
-                "lr_decay_every": args.lr_decay_every,
-                "feature_dim": args.feature_dim,
-                "embed_dim": args.embed_dim,
-                "iterations": args.iterations,
+                'seed': args.seed,
+                'experiment_name': args.experiment_name,
+                'dataset': args.dataset,
+                'data_dir': args.data_dir,
+                'flat': args.flat,
+                'bboxes': args.bboxes,
+                'subsample': args.subsample,
+                'rsd_type': args.rsd_type,
+                'fps': args.fps,
+                'train_split': args.train_split,
+                'test_split': args.test_split,
+                'batch_size': args.batch_size,
+                'iterations': args.iterations,
+                'network': args.network,
+                'backbone': args.backbone,
+                'load_backbone': args.load_backbone,
+                'feature_dim': args.feature_dim,
+                'embed_dim': args.embed_dim,
+                'dropout_chance': args.dropout_chance,
+                'pooling_layers': args.pooling_layers,
+                'roi_size': args.roi_size,
+                'load_experiment': args.load_experiment,
+                'load_iteration': args.load_iteration,
+                'optimizer': args.optimizer,
+                'loss': args.loss,
+                'momentum': args.momentum,
+                'betas': (args.beta1, args.beta2),
+                'lr': args.lr,
+                'weight_decay': args.weight_decay,
+                'lr_decay': args.lr_decay,
+                'lr_decay_every': args.lr_decay_every,
             },
         )
 
-    # TODO: Create Datasets
-    # - Feature Dataset (flat / sequential)
-    # - Image Dataset (flat / sequential)
+    # TODO: Subsampling
     if "features" in args.data_dir:
         trainset = FeatureDataset(
             data_root, args.data_dir, args.train_split, flat=args.flat
@@ -207,7 +227,7 @@ def main():
         testset, batch_size=args.batch_size, num_workers=4, shuffle=False
     )
 
-    # TODO: Create network
+    # TODO: Sequential RSDNet and ProgressNet
     if args.load_backbone:
         backbone_path = os.path.join(data_root, "train_data", args.load_backbone)
     else:
