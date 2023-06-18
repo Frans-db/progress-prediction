7ab80c83fb63308204b7ccc54b3e24c756463d2d
Frans de Boer <frans-de-booer@hotmail.com>
2023-06-18 14:36:19 +0000

    bars job

--- code/make_bars.py
+++ code/make_bars.py
@@ -36,29 +36,13 @@ def parse_args():
 
     return parser.parse_args()
 
-# R G B
-COLOURS = torch.Tensor([(219, 0, 91), (73, 66, 228), (208, 245, 190)]) / 255
+COLOURS = torch.Tensor([(251,75,75), (255,193,99), (254,255,92), (192,255,51)]) / 255
 
-def create_frame(num_notces, colour, notch_width: int, notch_height: int, size: int) -> torch.Tensor:
+def create_frame(notch_list: list[str], notch_width: int, notch_height: int, size: int) -> torch.Tensor:
     frame = torch.zeros(3, size, size)
-    for i in range(num_notces):
-        frame[:, 0:notch_height, i*notch_width:(i+1)*notch_width] = torch.Tensor(colour)[:, None, None]
+    for i, colour in enumerate(notch_list):
+        frame[:, 0:notch_height, i*notch_width:(i+1)*notch_width] = colour[:, None, None]
     return frame
-
-def lerp(value, start, end):
-    return start * (1 - value) + end * value
-
-def lerp_iterables(value, start, end):
-    new_iter = []
-    for (start_val, end_val) in zip(start, end):
-        new_iter.append(lerp(value, start_val, end_val))
-    return new_iter
-
-def lerp_colours(value: float, color1, color2, color3):
-    if value < 0.5:
-        return lerp_iterables(value / 0.5, color1, color2)
-    else:
-        return lerp_iterables((value - 0.5) / 0.5, color2, color3)
 
 def create_data(args):
     save_dir = os.path.join(args.root, args.save_dir)
@@ -67,8 +51,6 @@ def create_data(args):
     os.mkdir(os.path.join(save_dir, 'splitfiles'))
     transform = transforms.ToPILImage()
 
-    notches_per_video = args.actions_per_video * args.notches_per_action
-
     for video_index in tqdm(range(args.num_videos)):
         video_dir = os.path.join(save_dir, 'rgb-images', f'{video_index:05d}')
         os.mkdir(video_dir)
@@ -76,7 +58,7 @@ def create_data(args):
             video_multiplier = args.min_video_multiplier
         else:
             video_multiplier = random.randint(args.min_video_multiplier, args.max_video_multiplier)
-        num_notches = 0
+        notch_list = []
         frame_index = 0
         for action_index in range(args.actions_per_video):
             for notch_index in range(args.notches_per_action):
@@ -84,9 +66,8 @@ def create_data(args):
                     notch_length = args.min_frames_per_notch * video_multiplier
                 else:
                     notch_length = random.randint(args.min_frames_per_notch, args.max_frames_per_notch) * video_multiplier
-                num_notches += 1
-                colour = lerp_colours(num_notches / notches_per_video, COLOURS[0], COLOURS[1], COLOURS[2])
-                frame_array = create_frame(num_notches, colour, args.notch_width, args.notch_height, args.video_size)
+                notch_list.append(COLOURS[action_index])
+                frame_array = create_frame(notch_list, args.notch_width, args.notch_height, args.video_size)
                 frame = transform(frame_array)
 
                 for _ in range(notch_length):
@@ -115,15 +96,24 @@ def visualise(args):
         frames_per_video[video_name] = num_frames
         max_num_frames = max(num_frames, max_num_frames)
 
-    for i in tqdm(range(max_num_frames)):
+    for i in range(max_num_frames):
         frames = []
+        progress = []
         for video_name in video_names:
             frame_index = min(i, frames_per_video[video_name] - 1)
             frame_path = os.path.join(data_dir, video_name, f'{frame_index:05d}.jpg')
             frames.append(read_image(frame_path))
-        grid = make_grid(frames, nrow=4, padding=2, pad_value=255)
-        plt.figure(figsize=(8, 8))
-        plt.imshow(np.transpose(grid, [1, 2, 0]))
+            progress.append((i + 1) / frames_per_video[video_name])
+        fig, axs = plt.subplots(4, 4)
+        for frame, prog, ax in zip(frames, progress, axs.flat):
+            prog = min(1, prog)
+            ax.imshow(frame.T)
+            ax.axis('off')
+            ax.set_title(f'{(prog * 100):.1f}')
+            # ax.set_axis('off')
+
+        # plt.figure(figsize=(8, 8))
+        # plt.imshow(np.transpose(grid, [1, 2, 0]))
         plt.axis('off')
         plt.savefig(f'./plots/bars/{i:03d}.jpg')
         plt.clf()
